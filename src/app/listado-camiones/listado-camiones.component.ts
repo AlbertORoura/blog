c1ebe5a6247986699b76ac228b1cd1fb81024391
albert.oliva <albert.oliva@external.wolterskluwer.com>
2018-03-15 15:24:24 +0100

    tabla de  camiones

--- src/app/listado-camiones/listado-camiones.component.ts
+++ src/app/listado-camiones/listado-camiones.component.ts
@@ -1,4 +1,15 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs/Observable';
+
+import { IItems } from './items';
+import { AngularFireModule } from 'angularfire2';
+import { AngularFireDatabaseModule, AngularFireDatabase, AngularFireList } from 'angularfire2/database';
+//import { AngularFireDatabaseModule } from 'angularfire2/database';
+// for auth    
+import { AngularFireAuthModule } from 'angularfire2/auth';
+// for Observables
+import { FirebaseListObservable, FirebaseObjectObservable } from 'angularfire2/database-deprecated';
+//import { AngularFireDatabase } from 'angularfire2/database-deprecated';
 
 @Component({
   selector: 'pm-listado-camiones',
@@ -6,8 +17,18 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./listado-camiones.component.css']
 })
 export class ListadoCamionesComponent implements OnInit {
+  entornos: string;
+  items: Observable<IItems[]>;
+  itemsRef: AngularFireList<{}>;
+  // constructor() { }
 
-  constructor() { }
+  constructor(database: AngularFireDatabase) {    
+    this.itemsRef = database.list('Camiones');
+    // Use snapshotChanges().map() to store the key
+    this.items = this.itemsRef.snapshotChanges().map(changes => {
+      return changes.map(c => ({ key: c.payload.key, ...c.payload.val() }));
+    });
+  }
 
   ngOnInit() {
   }
